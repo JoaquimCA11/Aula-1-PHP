76506aae382e05f89ff504ac32ae7ebcc34e9e7d
JoaquimCA11 <joaquimarraes11@gmail.com>
2024-09-12 11:45:20 -0300

    Add files via upload

---
+++ login/default.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <?php
+        echo "Primeiro PHP!<br>";
+        $nome = "Gabriel";
+        $sobrenome = "Kreischer";
+        echo $nome . " " . $sobrenome;
+    ?>
+</body>
+</html>
